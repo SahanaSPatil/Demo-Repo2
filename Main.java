3731bc2189835b8e463555ca089bb1c129586590
SahanaSPatil <sahana17spatil@gmail.com>
2024-08-25 14:38:07 +0530

    created obj

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+class Car{
+
+    String company;
+    int model;
+
+    Car(String company , int model){
+        this.company = company;
+        this.model = model;
+
+       System.out.println("Created car of " + this.company + " model: " + this.model);
+    }
+
+}
+
 public class Main{
     public static void main(String argv[]){
         System.out.println("Hello World");
+
+        Car swift = new Car("Maruthi", 108);
     }
 }
